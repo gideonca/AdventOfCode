49c34042099716c544b8807158c3e887d69be34e
Cory Gideon <cgideon@wellabe.com>
2024-12-03 10:26:10 -0600

    Adding Day 3 code

---
+++ 2024/Day3.cs
@@ -0,0 +1,12 @@
+class Day3
+{
+    // TODO: Regex through text file
+    private string data = File.ReadAllText("./data/day3.txt");
+
+    public int ParseData()
+    {
+        int product = 0;
+
+        return product;
+    }
+}
