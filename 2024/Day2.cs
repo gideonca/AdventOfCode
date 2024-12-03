33bd6b90c6cd1c5518332131bf5134e9a5f29e8d
Cory Gideon <cgideon@wellabe.com>
2024-12-03 10:17:43 -0600

    Finished Day 2 challenge

---
+++ 2024/Day2.cs
@@ -0,0 +1,85 @@
+using System.ComponentModel.DataAnnotations;
+
+class Day2
+{
+
+    private string[] Lines = File.ReadAllLines("./data/day2.txt");
+
+    public int CheckSafetyWithoutDampener()
+    {
+        int numberOfSafeReactors = 0;
+        int safetyCount = 0;
+
+        foreach(var line in Lines)
+        {
+            int[] intArray = line.Split(" ").Select(int.Parse).ToArray();
+            numberOfSafeReactors += CheckSafety(intArray);
+        }
+
+        return numberOfSafeReactors;
+    }
+
+    public int CheckSafetyWithDampener()
+    {
+        int safety = 0;
+
+        foreach(var line in Lines)
+        {
+            int[] intArray = line.Split(" ").Select(int.Parse).ToArray(); // Source of truth array
+            int[] clonedArray = (int[])intArray.Clone(); // Use clone array to remove elements
+            
+            int isSafe = CheckSafety(clonedArray);
+            if(isSafe == 0) { // Initial array is not safe
+                for(int x = 0; x < clonedArray.Count(); x++)
+                {
+                    // Remove element by element, check for safety
+                    List<int> clone = clonedArray.ToList();
+                    clone.RemoveAt(x);
+                    if(CheckSafety(clone.ToArray()) == 1) {
+                        safety++;
+                        break;
+                    }
+                    clonedArray = (int[])intArray.Clone(); // reset cloned array
+                }
+            } else {
+                safety += CheckSafety(intArray);
+            }
+
+        }
+
+        return safety;
+    }
+
+    private int CheckSafety(int[] list)
+    {
+        int safe = 0;
+        int safetyCount = 0;
+
+        int order = GetListOrder(list);
+        if(order != 0) {
+            for(int x = 0; x < list.Count() - 1; x++)
+            {
+                int dif = Math.Abs(list[x] - list[x + 1]);
+                    
+                if(dif <= 3 && dif > 0)
+                {
+                    safetyCount++;
+                }
+            }
+
+            if(safetyCount == list.Count() - 1)
+                safe++;
+        }
+
+        return safe;
+    }
+
+    private int GetListOrder(int[] values)
+    {
+        if(values.SequenceEqual(values.Order()))
+            return 1;
+        else if(values.SequenceEqual(values.OrderDescending()))
+            return 2;
+        return 0;
+    }
+}
---
+++ 2024/Program.cs
@@ -0,0 +1,6 @@
+Day2 day2 = new Day2();
+// Without Dampener
+Console.WriteLine("Number safe reactors (no Dampener): " + day2.CheckSafetyWithoutDampener());
+
+// With Dampener
+Console.WriteLine("Number safe reactors (Dampener): " + day2.CheckSafetyWithDampener());
