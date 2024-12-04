bc7334f8651df53dc18e38ad10f481c67228b901
Cory Gideon <cgideon@wellabe.com>
2024-12-04 12:04:37 -0600

    Adding mapping of characters

--- 2024/Day4.cs
+++ 2024/Day4.cs
@@ -1,6 +1,6 @@
 class Day4
 {
-    private string[] data = File.ReadAllLines("./data/day4.txt");
+    private string[] data = File.ReadAllLines("./data/day4_demo.txt");
 
     public int CountWords_Phase1()
     {
@@ -10,6 +10,17 @@ class Day4
         int rows = data[0].Length;
 
         char[,] wordMatrix = new char[rows,cols];
+        var dataArray = data.ToArray();
+
+        // Populate character matrix
+        int row = 0;
+        foreach(var line in data)
+        {
+            for(int i = 0; i < line.ToArray().Length; i++)
+            {
+                wordMatrix[row, i] = line[i];
+            }
+        }
 
         int currentXPos = 0;
         int currentYPos = 0;
@@ -19,6 +30,9 @@ class Day4
         // look at top left corner (0,0)
         // check character, if it's X check adjacent (0,1) (1,0) (1,1) for M
         // check 
+        do{
+            
+        } while (currentXPos < rows && currentYPos < cols);
 
         return totalFound;
     }
